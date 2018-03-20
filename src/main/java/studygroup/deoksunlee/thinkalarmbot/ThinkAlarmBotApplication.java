c352f37409fda6093655b0bed607f632e5f42204
langve <langve0+github@gmail.com>
2018-03-20 16:18:57 +0900

    #1 프로젝트 생성

---
+++ src/main/java/studygroup/deoksunlee/thinkalarmbot/ThinkAlarmBotApplication.java
@@ -0,0 +1,12 @@
+package studygroup.deoksunlee.thinkalarmbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThinkAlarmBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThinkAlarmBotApplication.class, args);
+	}
+}
---
+++ src/test/java/studygroup/deoksunlee/thinkalarmbot/ThinkAlarmBotApplicationTests.java
@@ -0,0 +1,16 @@
+package studygroup.deoksunlee.thinkalarmbot;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ThinkAlarmBotApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
