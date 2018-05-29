617dff07275bf29364116bcf229befdfbed3ef7e
LSD <leesangdeok84@gmail.com>
2018-05-29 17:28:08 +0900

    #19 spring profiles 적용

--- src/main/java/studygroup/deoksunlee/thinkalarmbot/push/Push4Slack.java
+++ src/main/java/studygroup/deoksunlee/thinkalarmbot/push/Push4Slack.java
@@ -1,6 +1,7 @@
 package studygroup.deoksunlee.thinkalarmbot.push;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Value;
 import studygroup.deoksunlee.thinkalarmbot.util.HttpClientUtils;
 
 import java.io.IOException;
@@ -8,9 +9,23 @@ import java.net.URLEncoder;
 
 public class Push4Slack {
 
+    @Value("${property.slack.token}")
+    private static String token;
+
+    @Value("${property.slack.channel-id}")
+    private static String channelId;
+
+    public static String getToken() {
+        return token;
+    }
+
+    public static String getChannelId() {
+        return channelId;
+    }
+
     private static final String url = "https://slack.com/api/chat.postMessage?token=%s&channel=%s&text=%s&pretty=1";
-    private static final String token = "xoxp-340813866723-341964544615-340373455409-027937cfd9839e0e9254fd57b449f58e";
-    private static final String channelId = "CAP8PSQMP";
+//    private static final String token = "xoxp-340813866723-341964544615-340373455409-027937cfd9839e0e9254fd57b449f58e";
+//    private static final String channelId = "CAP8PSQMP";
 
 
     private static ObjectMapper mapper = new ObjectMapper(); // create once, reuse
--- src/test/java/studygroup/deoksunlee/thinkalarmbot/push/Push4SlackTest.java
+++ src/test/java/studygroup/deoksunlee/thinkalarmbot/push/Push4SlackTest.java
@@ -4,9 +4,13 @@ import org.junit.Assert;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 
 public class Push4SlackTest {
     private static final Logger logger = LoggerFactory.getLogger(Push4SlackTest.class);
+
+    @Autowired
+    Push4Slack push4Slack;
 
     @Test
     public void push() {
@@ -19,5 +23,21 @@ public class Push4SlackTest {
 
         // then
         Assert.assertTrue(response.isOk());
+    }
+
+    @Test
+    public void profileTest() {
+        // given
+
+        // when
+        String actualToken = Push4Slack.getToken();
+        String actualChannelId = Push4Slack.getChannelId();
+
+        // then
+        String expectedTocken = "xoxp-340813866723-341964544615-340373455409-027937cfd9839e0e9254fd57b449f58e";
+        String expectedChannelId = "CAP8PSQMP";
+
+        Assert.assertEquals(expectedTocken, actualToken);
+        Assert.assertEquals(expectedChannelId, actualChannelId);
     }
 }
