ec8aa2ec65e12f2cebd9b76d1719df6768b8afeb
langve <langve0+github@gmail.com>
2018-06-01 15:25:14 +0900

    #18 Controller 의 Unit Test 적용 예습

--- src/main/java/studygroup/deoksunlee/thinkalarmbot/controller/TabController.java
+++ src/main/java/studygroup/deoksunlee/thinkalarmbot/controller/TabController.java
@@ -1,5 +1,7 @@
 package studygroup.deoksunlee.thinkalarmbot.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import studygroup.deoksunlee.thinkalarmbot.crawler.CrawlerProcessorByHttpClient;
@@ -10,25 +12,32 @@ import studygroup.deoksunlee.thinkalarmbot.push.SlackChatPostMessageResponse;
 @RestController
 public class TabController {
 
+    private static final Logger logger = LoggerFactory.getLogger(TabController.class);
     private String URL = "http://localhost:8080/sample.xml";
 
     @PostMapping(value = "/api/check-and-push")
     public String checkAndPush() {
-        //crwaler
         String result = "{\"code\":200, \"message\":\"success\"}";
 
-        String xml = CrawlerProcessorByHttpClient.get(URL);
-        String xpath4EventNo = "//entrydata[@columnnumber=12]/text";
-        String eventNo = Parser4Xml.parse(xml, xpath4EventNo);
-        String xpath4EventTitle = "//entrydata[@columnnumber=3]/text";
-        String eventTitle = Parser4Xml.parse(xml, xpath4EventTitle);
+        try {
+            //crwaler
+            String xml = CrawlerProcessorByHttpClient.get(URL);
+            String xpath4EventNo = "//entrydata[@columnnumber=12]/text";
+            String eventNo = Parser4Xml.parse(xml, xpath4EventNo);
+            String xpath4EventTitle = "//entrydata[@columnnumber=3]/text";
+            String eventTitle = Parser4Xml.parse(xml, xpath4EventTitle);
 
-        // slack push
-        String message = String.format("새로운 %s번 이벤트[%s]가 등록되었습니다.", eventNo, eventTitle);
-        SlackChatPostMessageResponse response = Push4Slack.push(message);
+            // slack push
+            String message = String.format("새로운 %s번 이벤트[%s]가 등록되었습니다.", eventNo, eventTitle);
+            SlackChatPostMessageResponse response = Push4Slack.push(message);
 
-        if (!response.isOk())
-            result = String.format("{\"code\":500, \"message\":\"%s\"}", response.getError());
+            if (!response.isOk())
+                result = String.format("{\"code\":500, \"message\":\"slackAPI 호출 실패(%s)\"}", response.getError());
+        } catch (Exception e) {
+            String message = "정의되지 않은 오류 발생";
+            result = String.format("{\"code\":500, \"message\":\"%s\"}", message);
+            logger.error(message, e);
+        }
 
         return result;
     }
